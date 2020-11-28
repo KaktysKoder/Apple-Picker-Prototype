9edf91b1b8aefd0f45b81cd3a92f8db752c5c699
Kaktys Koder <kaktyskoder@gmail.com>
2020-11-28 14:11:28 +0400

    Реализована первая версия менеджера уровня ApplePicker - который генерирует три корзинки. Каждая из которых в будущем будет символизировать одну жизнь (одна корзин, один шанс на неудачу).

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -2,5 +2,31 @@
 
 public class ApplePicker : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    [SerializeField] private GameObject basketPrefab;
 
+    [SerializeField] int numBaskets = 3;
+
+    [SerializeField] private float basketBottomY = -14.0f;
+    [SerializeField] private float basketSpacingY = 2.0f;
+
+
+    private void Start()
+    {
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tempBasket = Instantiate(basketPrefab);
+
+            Vector3 tempBasketPosition = Vector3.zero;
+
+            tempBasketPosition.y = basketBottomY + (basketSpacingY * i);
+
+            tempBasket.transform.position = tempBasketPosition;
+        }
+    }
+
+    private void Update()
+    {
+
+    }
 }
