a5abf8f51d824446c271589929daddc93eb5f855
Kaktys Koder <kaktyskoder@gmail.com>
2020-11-28 13:50:06 +0400

    Реализована остановка падения яблок ниже заданного уровня.

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -2,5 +2,13 @@
 
 public class Apple : MonoBehaviour
 {
+    public static float BottomY = -20.0f;
 
+    private void Update()
+    {
+        if (transform.position.y < BottomY)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
