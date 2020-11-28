1a3121e8c5405f91f8efdaaf8e40134d1013752f
Kaktys Koder <kaktyskoder@gmail.com>
2020-11-28 11:57:33 +0400

    Инициализация параметров к сценарию AppleTree.

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -2,5 +2,21 @@
 
 public class AppleTree : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    [SerializeField] private GameObject applePrefab;
 
+    [SerializeField] private float appleTreeSpeed = 1.0f;
+    [SerializeField] private float leftAndRightEdge = 10.0f;
+    [SerializeField] private float chanceToChangeDirections = 0.1f;
+    [SerializeField] private float secondBetweenAppleDrops = 1.0f;
+
+    private void Start()
+    {
+        // TODO: Сбрасывать яблоки раз в секунду.
+    }
+
+    private void Update()
+    {
+        // TODO: Простое перемещение и изменение направления.
+    }
 }
